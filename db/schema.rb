ea299a61e5be45fcde176ce1d81537cc77cd2483
Eric Ebbesen <eebbesen.git@gmail.com>
2016-01-18 18:41:36 -0600

    Added Postings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160119003829) do
+ActiveRecord::Schema.define(version: 20160119004027) do
 
   create_table "bios", force: :cascade do |t|
     t.text     "name"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 20160119003829) do
     t.text     "filename"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "postings", force: :cascade do |t|
+    t.text     "years"
+    t.text     "position"
+    t.text     "post"
+    t.text     "district"
+    t.text     "hbca_reference"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
 end
